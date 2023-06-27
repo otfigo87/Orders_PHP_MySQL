54207148ae70bd09c388597a1aaaf4c4447af895
Otmane Aatik <aaotmane@gmail.com>
2023-06-27 19:18:05 -0400

    Design first commit

---
+++ admin/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Restaurant Website</title>
+    <link rel="stylesheet" href="../css/admin.css" />
+</head>
+
+<body>
+    <!-- Menu Section -->
+    <div class="menu">
+        <div class="wrapper">
+            Menu Here
+        </div>
+    </div>
+
+    <!-- Main content -->
+    <div class="main-content">
+        <div class="wrapper">
+            Content
+        </div>
+    </div>
+
+    <!-- Footer Section -->
+    <div class="footer">
+        <div class="wrapper">
+            Footer
+        </div>
+    </div>
+
+</body>
+
+</html>
