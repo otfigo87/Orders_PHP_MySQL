771c22e71815979dc5ece3d30ee58e6acc23d75d
Otmane Aatik <aaotmane@gmail.com>
2023-07-02 13:37:33 -0400

    CMS tables

--- admin/manage-admin.php
+++ admin/manage-admin.php
@@ -1,11 +1,43 @@
 <?php include('partials/menu.php'); ?>
 
-    <!-- Main content -->
-    <div class="main-content">
-        <div class="wrapper boxes">
-            <strong>Manage Admin</strong>
-        </div>
-    </div>
+<!-- Main content -->
+<div class="main-content">
+    <div class="wrapper">
+        <h1>Manage Admin</h1>
+        <br />
 
-    <!-- Footer Section -->
-   <?php include('partials/footer.php'); ?>
+        <a href="#" class="btn-primary">Add Admin</a>
+        <br /><br /><br />
+
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>Otmane</td>
+                <td>otmane</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>Otmane</td>
+                <td>otmane</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+        </table>
+    </div>
+</div>
+
+<!-- Footer Section -->
+<?php include('partials/footer.php'); ?>
--- admin/manage-order.php
+++ admin/manage-order.php
@@ -3,6 +3,36 @@
 <div class="main-content">
     <div class="wrapper">
         <h1>Manage Orders</h1>
+
+        <br /><br /><br />
+
+        <table class="tbl-full">
+            <tr>
+                <th>S.N</th>
+                <th>Full Name</th>
+                <th>Username</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>Otmane</td>
+                <td>otmane</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+            <tr>
+                <td>1.</td>
+                <td>Otmane</td>
+                <td>otmane</td>
+                <td>
+                    <a href="#" class="btn-secondary">Update Admin</a>
+                    <a href="#" class="btn-danger">Delete Admin</a>
+                </td>
+            </tr>
+
+        </table>
     </div>
 </div>
 
