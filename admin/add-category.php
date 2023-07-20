c29feac241fc3cfaf2519cd4a2dbe7a841963200
Otmane Aatik <aaotmane@gmail.com>
2023-07-20 11:10:56 -0400

    upload category image

--- admin/add-category.php
+++ admin/add-category.php
@@ -11,15 +11,25 @@
             echo $_SESSION['add']; 
             unset($_SESSION['add']); // Remove Session message
         }
+        if (isset($_SESSION['upload'])) {
+            echo $_SESSION['upload'];
+            unset($_SESSION['upload']); // Remove Session message
+        }
         ?>
 
 
-        <form action="" method="POST">
+        <form action="" method="POST" enctype="multipart/form-data">
             <table class="tbl-30">
                 <tr>
                     <td>Title: </td>
                     <td>
                         <input type="text" name="title" placeholder="Category Title">
+                    </td>
+                </tr>
+                <tr>
+                    <td>Select Image: </td>
+                    <td>
+                        <input type="file" name="image">
                     </td>
                 </tr>
                 <tr>
@@ -61,9 +71,31 @@
                 $active = "No";
             }
 
+            // Check if image selected
+            // print_r($_FILES['image']);
+            // die(); 
+
+            if(isset($_FILES['image']['name'])){
+                //Upload image
+                $image_name = $_FILES['image']['name'];
+                $source_path = $_FILES['image']['tmp_name'];
+                $destination_path = "../images/category/" . $image_name;
+                $upload = move_uploaded_file($source_path, $destination_path);
+
+                if($upload == false){
+                    $_SESSION['upload'] = "<div class='error'>Failed to Upload Image.</div>";
+                    header('location:' . SITE_URL . 'admin/add-category.php');
+                    die();
+                }
+            }else {
+                // No Image
+                $image_name = "";
+            }
+
             //SQL Queries
             $sql = "INSERT INTO table_category SET 
             title='$title',
+            image_name='$image_name',
             featured='$featured',
             active='$active'
             ";
@@ -73,7 +105,7 @@
             if($res == true){
                 $_SESSION['add'] = "<div class='success'>Category Added Successfully.</div>";
                 header('location:'.SITE_URL.'admin/manage-category.php');
-            }else {
+            } else {
                 $_SESSION['add'] = "<div class='error'>Failed to Add.</div>";
                 header('location:' . SITE_URL . 'admin/add-category.php');
             }
